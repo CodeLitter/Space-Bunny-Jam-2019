3a25bac2f0086f7989e097d3c56dc2b92a2189ed
Issac <iirlas@yahoo.com>
2019-02-02 21:27:54 -0600

    Revised Assets\Scripts\PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,7 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 10.0f; //public variable will have default values if possible 
     [HideInInspector] public Rigidbody2D rigidbody2D; //Hidden public come after visible variables
-    private Vector3 m_velocity; //private variables will be prefixed with m_ and will be last in the variable listing
+    private Vector2 m_velocity = Vector2.zero; //private variables will be prefixed with m_ and will be last in the variable listing
 
     //Use awake to initialize local components within this gameobject
     private void Awake()
@@ -17,15 +17,17 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Preserve gravity
+        m_velocity.y = rigidbody2D.velocity.y;
         //Apply velocity in fixedUpdate
-        rigidbody2D.velocity = m_velocity * speed;
+        rigidbody2D.velocity = m_velocity;
     }
 
     //Use update when capturing input as it is framerate dependent
     private void Update()
     {
         //Save captured input, horizontal movement is only needed
-        m_velocity.x = Input.GetAxis("Horizontal");
+        m_velocity.x = Input.GetAxis("Horizontal") * speed;
     }
 
 
