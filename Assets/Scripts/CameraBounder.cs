16ea3216c636cc2cb12fab30b2893c0d9519795b
Issac <iirlas@yahoo.com>
2019-02-02 20:42:24 -0600

    Added Assets\Scripts\CameraBounder.cs Added Assets\Scripts\CameraTracker.cs

---
+++ Assets/Scripts/CameraBounder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")]
+public class CameraBounder : CinemachineExtension
+{
+    public Collider2D boundingShape;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Finalize)
+        {
+            var bounds = boundingShape.bounds;
+            var target_position = vcam.Follow.position;
+            target_position.x = Mathf.Clamp(target_position.x, bounds.min.x, bounds.max.x);
+            vcam.Follow.position = target_position;
+        }
+    }
+}
---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")]
+public class CameraTracker : CinemachineExtension
+{
+    public Vector3 offset;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var position = vcam.Follow.position;
+            position.x = 0.0f;
+            state.RawPosition = position + offset;
+        }
+    }
+}
