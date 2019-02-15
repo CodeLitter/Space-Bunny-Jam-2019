d712f1eeb54b5d3e4ca5b770422f196de92906d6
Issac <iirlas@yahoo.com>
2019-02-15 16:40:31 -0600

    Added Assets\Scripts\Attacher.cs

---
+++ Assets/Scripts/Attacher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacher : MonoBehaviour
+{
+    public GameObject prefab;
+    [Tag] public string targetTag;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            Instantiate(prefab, other.transform);
+        }
+    }
+}
