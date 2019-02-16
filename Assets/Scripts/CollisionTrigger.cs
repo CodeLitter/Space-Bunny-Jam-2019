d923ebe1c5ba8db13fb58d73168e2a629cbd607a
Issac <iirlas@yahoo.com>
2019-02-15 22:15:18 -0600

    Added Assets\Scripts\SceneManager.cs
    Revised Assets\Scripts\CollisionTrigger.cs

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -7,6 +7,14 @@ public class CollisionTrigger : MonoBehaviour
     [Tag] public string colliderTag = "Untagged";
     public UnityEngine.Events.UnityEvent collisionEvent;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag(colliderTag))
+        {
+            collisionEvent.Invoke();
+        }
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         if (other.CompareTag(colliderTag))
